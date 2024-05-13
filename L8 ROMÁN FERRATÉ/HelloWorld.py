c255159b3dce4e3732bfca89b7c0b179a6727e7c
RomanFC22022 <rferratecasasola@gmail.com>
2024-05-13 05:47:25 -0600

    Lecciones Phyton

---
+++ L8 ROMÁN FERRATÉ/HelloWorld.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre \n")
+print("Hola", nombre)
+
+numero1 = int(input("ingrese un numero \n"))
+numero2 = int(input("ingrese otro numero \n"))
+print(numero1 + numero2)
---
+++ L9 ROMÁN FERRATE/L9RománFerraté1103224.py
@@ -0,0 +1,29 @@
+#Ejercicio 1
+print("Ejercicio 1: operaciones aritmeticas")
+
+#Entradas
+numero1 = int(input("Ingresa un numero"))
+numero2 = int(input("Ingresa otro un numero"))
+
+#Operaciones
+suma = numero1 + numero2
+resta = numero1 - numero2
+divisionEntera = numero1 // numero2
+divisionModular = numero1 % numero2
+multiplicacion = numero1 * numero2
+
+#Salidas
+print(numero1, "//", numero2, "=" ,divisionEntera)
+print(numero1, "%", numero2, "=" ,divisionModular)
+print("Suma" ,suma)
+print("Resta" ,resta)
+print("Multiplicacion" ,multiplicacion)
+
+
+#Ejercicio 2
+print("Ejercicio 2: operaciones boleanas")
+diferencia = numero1 != numero2
+print(numero1, "!=", numero2, "=", diferencia)
+
+
+#Ejercicio 3
